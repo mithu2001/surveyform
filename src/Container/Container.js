1f7a8734ff645ef5b1df0fd5458ad84f44ff8f9c
mithu2001 <guptamithu2019@gmail.com>
2023-01-04 23:35:32 +0530

    created Component

---
+++ src/Container/Container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Container extends Component {
+    constructor(props){
+        super(props);
+        this.state= {};
+    }
+  render() {
+    return (
+      <div>Container</div>
+    )
+  }
+}
+
+export default Container
